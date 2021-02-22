f56024b9c0b3d16f249e3bf097f72c7410ba9872
SameerAMohamed <36165297+SameerAMohamed@users.noreply.github.com>
2021-02-22 12:53:47 -0600

    Add files via upload
    Initial commit

---
+++ main.py
@@ -0,0 +1,197 @@
+import datetime
+import time
+
+import alpaca_trade_api as tradeapi
+import pandas as pd
+import regex as re
+import requests
+from nltk.corpus import stopwords
+
+from methodsFile import order_stuff
+from sentiment import execute_sentiment, sentiment_analysis
+
+runcount = 1
+# while market_open == True:
+# twitter authentication
+key = 'DH33Fyb6sSslFzV2zEbuHiKLn'
+keySecret = 'Qw5i5xzDAMgnN4O1nSg58jMFKgk03CTfaKLoP648po1VsE4U8X'
+bearerToken = 'AAAAAAAAAAAAAAAAAAAAAAVVIwEAAAAAv1Nr3WPg%2BkPaHNusA9Xgj1CGuig' \
+              '%3DT6fqiFJchskx3gJnmAnSr9TLig4CbjvCvvQTZKJKLpNvwbuO7r '
+auth = bearerToken
+currentTime = time.time()
+# Authentication stuff for Alpaca
+base_endpoint = 'https://paper-api.alpaca.markets'
+api_key_id = 'PKKSZY7HUXSG54X8GV2Q'
+secret_key = 'WdqcOFVTPoPYxzzV0q8UW76ad0rpcjfz0vEucaj5'
+
+api = tradeapi.REST(api_key_id, secret_key, base_url=base_endpoint)  # or use ENV Vars shown below
+account = api.get_account()
+clock = api.get_clock()
+
+# Read in all common names of brands from csvs
+commonNamesDF = pd.read_csv('sp500name.csv', encoding='ISO-8859-1')
+commonNamesList = commonNamesDF.values.tolist()
+# Read in all tickers of brands from csvs
+commonTickersDF = pd.read_csv('sp500ticker.csv', encoding='ISO-8859-1')
+commonTickersList = commonTickersDF.values.tolist()
+# _____________________________________________________________________________________________________________________
+headers = {"Authorization": f"Bearer {bearerToken}"}
+data = {"ip": "1.1.2.3"}
+woid_us = 23424977
+woid_world = 1
+print('-------------------------------------------------------------------------------------')
+endpoint = f"https://api.twitter.com/1.1/trends/place.json?id={woid_us}"
+# Train the sentiment analysis model
+before_train_time = time.time()
+classifier = sentiment_analysis()
+print('It took ', time.time() - before_train_time, ' seconds to train the sentiment model.')
+while 1 == 1:
+    clock = api.get_clock()
+    is_market_open = clock.is_open
+    while is_market_open == True:  # Changed to False for after-hours testing
+        try:
+            # Find out what time close will happen - CALL CLOCK FEWER TIMES
+            end_timer = time.time()
+            if runcount == 0 or runcount == 1:
+                clock = api.get_clock()
+                closingTime = clock.next_close.replace(tzinfo=datetime.timezone.utc).timestamp()
+                currTime = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
+                timeToClose = closingTime - currTime
+                start_timer = time.time()
+            # Sell when it's 5 minutes to close
+            if (timeToClose - (end_timer - start_timer)) < (60 * 5) and timeToClose > 0:
+                while is_market_open == True:
+                    print("Market closing soon.  Closing positions.")
+
+                    order_stuff().close_all_positions()
+                    clock = api.get_clock()
+                    is_market_open = clock.is_open
+                    time.sleep(5)
+            if is_market_open == False:
+                break
+
+            start_time = time.time()
+            us_trends = requests.get(endpoint, headers=headers).json()
+            us_trends = us_trends[0]
+            # us_trends['trends'][25]['name'] = 'Google'#Use this for testing ttttesting
+            stop_words = stopwords.words('english')
+
+            # commonNamesList[1][0] = 'Happy'#USE THIS FOR TESTING. DELETE LATER
+            counter = 0
+            counterMeasure = 0
+            index = 0
+            indexList = []
+            for term in commonNamesList:
+                term = term[0].lower()
+                for i in range(0, len(us_trends['trends'])):
+                    counterMeasure = counterMeasure + 1
+                    # Split us_trends['trends'][i]['name'].lower() by space
+                    subWords = us_trends['trends'][i]['name'].lower().split(' ')
+                    if term in subWords:  # or us_trends['trends'][i]['name'].lower().startswith(term): DON'T KNOW IF
+                        # I CAN GET THIS TO WORK PROPERLY
+                        counter = counter + 1
+                        indexList.append(index)
+                index = index + 1
+
+            # Filter out any replicates in indexList
+            indexList = list(dict.fromkeys(indexList))
+
+            tickerList = []
+            nameList = []
+            # We must identify each ticker for the company. We have organized the CSVs so they should have the same
+            # index
+            for placements in indexList:  # we generate tickerList which is a list of all the tickers that are trending
+                tickerList.append(commonTickersList[placements])
+                nameList.append(commonNamesList[placements])
+
+            # Now that we have the indices of the companies that are trending, we must search for each,
+            # sample n tweets, and analyze them for sentiment We will parse tweets that are searched for into a list
+            # of strings before here sentiment.py will be used to analyze their sentiments
+            tweetTexts = []
+            tweetTexts2 = []
+            for names in nameList:
+                names = names[0]
+                numberOfTweets = 100  # Up to 100, but does not always have data to take
+                searchEndpoint = f'https://api.twitter.com/1.1/search/tweets.json?q={names}&count={numberOfTweets}&lang=en&result_type=popular'  # change pfizer to company name after testing
+                searchedTweets = requests.get(searchEndpoint, headers=headers).json()
+
+                tweetTexts = []
+                for i in range(0, len(searchedTweets['statuses'][:]) - 1):
+                    tweetTexts.append(searchedTweets['statuses'][i]['text'])
+
+                for i in range(0, len(tweetTexts)):
+                    tweetHolder = tweetTexts[i].split(': ')  # split into before and after the colon
+                    if len(tweetHolder) > 1:
+                        tweetTexts[i] = tweetHolder[1]
+                    tweetTexts[i] = re.sub('\W+', ' ', tweetTexts[i])
+                tweetTexts2.append(tweetTexts)
+
+            # tweetTexts is now a SENSIBLE list of all strings to be processed for
+            # sentiment----------------------------------------------------- Find the sentiment for the tweet texts
+            totalSentiment = []
+            length_of_tweetTexts = len(tweetTexts2)
+            # Generate a list of tickers we have bought
+            portfolio_ticker_list = []
+            portfolio = api.list_positions()
+            for position in portfolio:
+                portfolio_ticker_list.append(position.symbol)
+            for i in range(0, len(tweetTexts2)):
+                ticker = tickerList[i][0]
+                if tickerList[i] not in portfolio_ticker_list:
+                    print(f'Calculating total sentiment for {tickerList[i]}...')
+                    before_time = time.time()
+                    totalSentiment.append(execute_sentiment(tweetTexts2[i], classifier))
+                    print(f'total sentiment is {totalSentiment}')
+                    print('it took ', (time.time() - before_time), 'seconds to determine the sentiment of this topic')
+            # We now have totalSentiment as a measure of the sentiment around that brand. Now we must place an order
+            # based on the sentiment
+
+            for i in range(0, len(tickerList)):
+                if i == len(tickerList):
+                    break
+                targetTicker = tickerList[i][0]
+                # totalSentiment = [0.5]# Use for testing ttttesting
+                if targetTicker not in portfolio_ticker_list:
+                    if totalSentiment[i] > 0.1:
+                        print(
+                            f"{targetTicker} has surpassed the sentiment threshold to buy and is not in the "
+                            f"portfolio. Attempting to buy {targetTicker}.")
+                        order_stuff().buying(targetTicker, portfolio_ticker_list)
+                    if totalSentiment[i] < -0.65:  # Look for what targetTicker and totalSentiment are
+                        print(
+                            f"{targetTicker} has surpassed the sentiment threshold to short and is not in the "
+                            f"portfolio. Attempting to short {targetTicker}.")
+                        order_stuff().shorting(targetTicker, portfolio_ticker_list)
+
+            # ---------------------------------------------------------------------------------------------------------
+            print("My program took", time.time() - start_time, "to run for the", runcount, " time")
+            if (time.time() - start_time) < 30:
+                print('sleeping until 30 seconds...')
+                time.sleep(30 - (time.time() - start_time))
+
+            runcount = runcount + 1
+        except:
+            print("There was an issue collecting or analyzing data. Sleeping for 10 seconds and retrying.")
+            time.sleep(10)
+    while is_market_open == False:
+        openingTime = clock.next_open.replace(tzinfo=datetime.timezone.utc).timestamp()
+        currTime = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
+        timeToOpen = openingTime - currTime
+
+        if timeToOpen > 300:
+            x_time = timeToOpen - 300.01
+        else:
+            x_time = 0.5
+        print('Market is not open... Checking again in ', x_time, ' seconds')
+        clock = api.get_clock()
+        is_market_open = clock.is_open
+        time.sleep(x_time)
+        runcount = 0
+        clock = api.get_clock()
+        is_market_open = clock.is_open
+        if timeToOpen < 0:
+            is_market_open = True
+        if is_market_open == True:
+            print("Market has opened")
+        else:
+            print("Market is not open")
---
+++ methodsFile.py
@@ -0,0 +1,184 @@
+import alpaca_trade_api as tradeapi
+import time
+import threading
+import math
+
+class order_stuff:
+    def __init__(self):
+        API_KEY = 'PKKSZY7HUXSG54X8GV2Q'
+        API_SECRET = 'WdqcOFVTPoPYxzzV0q8UW76ad0rpcjfz0vEucaj5'
+        APCA_API_BASE_URL = 'https://paper-api.alpaca.markets'
+        self.alpaca = tradeapi.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, 'v2')
+
+        self.long = []
+        self.short = []
+        self.qShort = None
+        self.qLong = None
+        self.adjustedQLong = None
+        self.adjustedQShort = None
+        self.blacklist = set()
+        self.longAmount = 0
+        self.shortAmount = 0
+        self.timeToClose = None
+    # Make a method to close all positions
+    def close_all_positions(self):
+        base_endpoint = 'https://paper-api.alpaca.markets'
+        api_key_id = 'PKKSZY7HUXSG54X8GV2Q'
+        secret_key = 'WdqcOFVTPoPYxzzV0q8UW76ad0rpcjfz0vEucaj5'
+        api = tradeapi.REST(api_key_id, secret_key, base_url=base_endpoint)  # or use ENV Vars shown below
+
+        positions = api.list_positions()
+        for position in positions:
+            if (position.side == 'long'):
+                orderSide = 'sell'
+            else:
+                orderSide = 'buy'
+            qty = abs(int(float(position.qty)))
+            respSO = []
+            try:
+                tSubmitOrder = threading.Thread(target=api.submit_order(position.symbol, int(float(qty)), orderSide, 'market', 'day'))
+                tSubmitOrder.start()
+                tSubmitOrder.join()
+            except:
+                print("There was a problem closing the positions")
+
+
+    def buying(self, targetTicker, portfolio_ticker_list):
+        if targetTicker not in portfolio_ticker_list:
+            base_endpoint = 'https://paper-api.alpaca.markets'
+            api_key_id = 'PKKSZY7HUXSG54X8GV2Q'
+            secret_key = 'WdqcOFVTPoPYxzzV0q8UW76ad0rpcjfz0vEucaj5'
+
+            api = tradeapi.REST(api_key_id, secret_key, base_url=base_endpoint)  # or use ENV Vars shown below
+
+            last_quote = api.get_last_quote(targetTicker)
+            share_price = last_quote.askprice
+
+            account = api.get_account()
+            cash = account.cash
+            portfolio = api.list_positions()
+            number_of_equities = len(portfolio)
+            # If there are no current securities, use half of our cash to buy the targetTicker
+            if number_of_equities == 0 or number_of_equities == 1:
+                amount_to_invest = int(float(cash)) / 2
+                share_count_to_buy = amount_to_invest / float(share_price)
+                try:
+                    tSubmitOrder = threading.Thread(target=api.submit_order(targetTicker, share_count_to_buy, 'buy', 'market', 'day'))
+                    print('Buying ',share_count_to_buy,' shares of ',targetTicker,'at ',share_price,' per share')
+                    tSubmitOrder.start()
+                    tSubmitOrder.join()
+                except:
+                    print("There was a problem placing the order")
+            # If there is 1 current security, use the rest of our cash to buy the other item of interest
+            if number_of_equities == 0:
+                amount_to_invest = cash
+                share_count_to_buy = amount_to_invest / float(share_price)
+                try:
+                    tSubmitOrder = threading.Thread(target=api.submit_order(targetTicker, share_count_to_buy, 'buy', 'market', 'day'))
+                    print('Buying ', share_count_to_buy, ' shares of ', targetTicker, 'at ', share_price, ' per share')
+                    tSubmitOrder.start()
+                    tSubmitOrder.join()
+                except:
+                    print("There was a problem placing the order")
+            # If there is more than 1 security, split the equity evenly between them
+            if number_of_equities > 1:
+                tickers = [] # First generate a list of all tickers you want to hold
+                for position in portfolio:
+                    tickers.append(position.symbol)
+                tickers.append(targetTicker)
+                #Now that we have a list, we must close all positions
+                positions = api.list_positions()
+                for position in positions:
+                    if (position.side == 'long'):
+                        orderSide = 'sell'
+                    else:
+                        orderSide = 'buy'
+                    qty = abs(int(float(position.qty)))
+                    respSO = []
+                    try:
+                        tSubmitOrder = threading.Thread(target=api.submit_order(position.symbol, int(float(qty)), orderSide, 'market', 'day'))
+                        print('Submitting order for', qty, 'shares of ',position.symbol,' to ', orderSide,'.')
+                        tSubmitOrder.start()
+                        tSubmitOrder.join()
+                    except:
+                        print("There was a problem placing the order")
+                #Now we reopen all desired positions
+                for symbols in tickers:
+                    amount_to_invest = float(cash) / len(tickers)
+                    share_count_to_buy = amount_to_invest / float(share_price)
+                    share_count_to_buy = int(float(math.floor(share_count_to_buy)))
+                    try:
+                        tSubmitOrder = threading.Thread(target=api.submit_order(symbols, share_count_to_buy, 'buy', 'market', 'day' ))
+                        print('Submitting order for', share_count_to_buy, 'shares of ',symbols,' to buy.')
+                        tSubmitOrder.start()
+                        tSubmitOrder.join()
+                    except:
+                        print("There was a problem placing the order")
+        else:
+            print('Already invested in target security')
+
+    def shorting(self, targetTicker, portfolio_ticker_list):
+        if targetTicker not in portfolio_ticker_list:
+            #if __name__ == '__main__':
+            symbol = targetTicker
+            """
+            With the Alpaca API, you can open a short position by submitting a sell
+            order for a security that you have no open position with.
+            """
+
+            # First, open the API connection
+            api = tradeapi.REST(
+                'PKKSZY7HUXSG54X8GV2Q',
+                'WdqcOFVTPoPYxzzV0q8UW76ad0rpcjfz0vEucaj5',
+                'https://paper-api.alpaca.markets', api_version='v2'
+            )
+
+            #Identify how many stocks we want to short
+            #First we get how many equities we have
+            account = api.get_account()
+            cash = account.cash
+            portfolio = api.list_positions()#Probably check out what portfolio looks like
+            number_of_equities = len(portfolio)
+            #Calculate how much we can short using 1/4 of the remaining cash
+            cash_to_short = float(cash)/4
+            #Get a price on our symbol
+            symbol_bars = api.get_barset(symbol, 'minute', 1).df.iloc[0]
+            symbol_price = symbol_bars[symbol]['close']
+
+            shares_to_short = cash_to_short/symbol_price
+            shares_to_short = int(float(math.floor(shares_to_short)))
+            # The security we'll be shorting
+            symbol = targetTicker
+
+            # Submit a market order to open a short position of one share
+            try:
+                order = api.submit_order(symbol, shares_to_short, 'sell', 'market', 'day')
+                print("first order submitted")
+                print('Shorting ', shares_to_short, ' shares of ', symbol)
+            except:
+                print("There was an error while attempting to place order...")
+
+            # Submit a limit order to attempt to grow our short position
+            # First, get an up-to-date price for our symbol
+            symbol_bars = api.get_barset(symbol, 'minute', 1).df.iloc[0]
+            symbol_price = symbol_bars[symbol]['close']
+            # Submit an order for one share at that price
+            try:
+                order = api.submit_order(symbol, shares_to_short, 'sell', 'limit', 'day', symbol_price)
+                print("Limit order submitted.")
+            except:
+                print("There was an error while attempting to place order...")
+
+            # Wait a second for our orders to fill...
+            print('Waiting...')
+            time.sleep(1)
+
+            # Check on our position
+            try:
+                position = api.get_position(symbol)
+                if int(float(position.qty)) < 0:
+                    print(f'Short position open for {symbol}')
+            except:
+                print("There was an error checking our position")
+        else:
+            print('Already invested in target security')
